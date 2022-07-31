0963caa9fc1b318a1b5021f538e3461038eff7d7
biggreenrm <biggreen.rm@gmail.com>
2022-07-31 15:57:05 +0300

    [IMP] main: add templates for best books and thrillers, add pagination.

--- reader/routes.py
+++ reader/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, send_from_directory
+from flask import render_template, request, send_from_directory
 
 from reader import app
 from reader.models import Book
@@ -7,8 +7,23 @@ from reader.models import Book
 
 @app.route("/")
 def index():
-    books = Book.query.all()
+    page = request.args.get("page", 1, type=int)
+    books = Book.query.order_by(Book.created_at.desc()).paginate(page=page, per_page=4)
     return render_template("index.html", books=books)
+
+
+@app.route("/thrillers/")
+def thrillers():
+    page = request.args.get("page", 1, type=int)
+    books = Book.query.filter(Book.genre == "триллер").paginate(page=page, per_page=4)
+    return render_template("thrillers.html", books=books)
+
+
+@app.route("/best/")
+def best():
+    page = request.args.get("page", 1, type=int)
+    books = Book.query.filter(Book.rating > 4).paginate(page=page, per_page=4)
+    return render_template("thrillers.html", books=books)
 
 
 @app.route("/uploads/<filename>")
